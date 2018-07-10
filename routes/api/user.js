52aa0b91ba122efafc0764a32f64f18778a7b6db
Bohdan Rusinka <bohdan.rusinka@binary-studio.com>
2018-07-10 08:29:38 +0300

    Nodejs demo: initial commit

---
+++ routes/api/user.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const userService = require("../../services/user");
+
+router.get("/", (req, res, next) => {
+  userService.findAll((err, data) => {
+    if (!err) {
+      res.data = data;
+      res.json(res.data);
+    } else {
+      res.status(400);
+      res.end();
+    }
+  });
+});
+
+router.get("/:id", (req, res, next) => {
+  userService.findOne(Number(req.params.id), (err, data) => {
+    if (!err) {
+      res.data = data;
+      res.json(res.data);
+    } else {
+      res.status(400);
+      res.end();
+    }
+  });
+});
+
+module.exports = router;
